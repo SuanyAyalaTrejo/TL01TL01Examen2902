7e6a5dc73a217af07b5565a72068b079524eeee0
Suany Ayala Trejo <suamat10@gmail.com>
2023-03-19 00:00:09 -0600

    Cambios al 18/03/2023

--- app/src/main/java/com/example/tl01examen2902/ActivitySegunda.java
+++ app/src/main/java/com/example/tl01examen2902/ActivitySegunda.java
@@ -1,18 +1,25 @@
 package com.example.tl01examen2902;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tl01examen2902.configuracion.SQLiteConnection;
@@ -23,31 +30,67 @@ public class ActivitySegunda extends AppCompatActivity {
     EditText Nombre, Telefono, Acerca;
 
     Spinner Pais;
-    Button btnGuardar, btnCancelar;
+    Button btnGuardar;
+    Button btnCancelar;
+    ImageButton btnimagen;
+    private static final int PICK_IMAGE = 100;
+    ImageView imgAvatar;
+Uri ImageUri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
         try {
 
 
+            imgAvatar = (ImageView) findViewById(R.id.imgAvatar);
 
-        Nombre = (EditText) findViewById(R.id.txtNombre);
-        Telefono = (EditText) findViewById(R.id.txtTelefono);
-        Pais = (Spinner) findViewById(R.id.cmbPais);
-        Acerca = (EditText) findViewById(R.id.txtAcerca);
-        btnGuardar = (Button) findViewById(R.id.btnGuardar);
-        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+            Nombre = (EditText) findViewById(R.id.txtNombre);
+            Telefono = (EditText) findViewById(R.id.txtTelefono);
+            Pais = (Spinner) findViewById(R.id.cmbPais);
+            Acerca = (EditText) findViewById(R.id.txtAcerca);
+            btnGuardar = (Button) findViewById(R.id.btnGuardar);
+            btnCancelar = (Button) findViewById(R.id.btnCancelar);
+
+            ActivityResultLauncher<Intent> camaraLaucher= registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode()==RESULT_OK);
+                    Bundle extras = result.getData().getExtras();
+                    Bitmap imgbitma = (Bitmap) extras.get("data");
+                    imgAvatar.setImageBitmap( imgbitma);
+                }
+            });
+            btnimagen.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view){
+                    camaraLaucher.launch (new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                }
+            });
+
+            btnGuardar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AgregarContactos();
+
+                }
+            });
 
 
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AgregarContactos();
 
-            }
-        });
-    }
+
+
+        }
+
+
+
+
+
+
+
         catch (Exception ex)
         {
             Toast.makeText(this, ex.toString(),Toast.LENGTH_SHORT).show();
@@ -60,13 +103,22 @@ public class ActivitySegunda extends AppCompatActivity {
         Pais.setAdapter(adapter);
 
 
+
+
         btnCancelar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 CancelarAgregar();
             }
         });
+
+
+
+
     }
+
+
+
 
     private void CancelarAgregar() {
         CleanScreen();
@@ -104,5 +156,5 @@ public class ActivitySegunda extends AppCompatActivity {
 
     }
 
-
 }
+
